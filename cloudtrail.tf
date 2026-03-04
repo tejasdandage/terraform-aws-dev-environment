3a1ee547f7712f5ebb5800ad26f01583949b1b0b
Tejas Dandage <tejasdandage@Tejass-MacBook-Air.local>
2026-03-04 11:28:16 +0530

    Added CloudTrail setup with S3 bucket using Terraform

---
+++ cloudtrail.tf
@@ -0,0 +1,68 @@
+resource "aws_s3_bucket" "cloudtrail_bucket" {
+  bucket = "tejas-cloudtrail-logs-bucket-12345"
+
+  tags = {
+    Name        = "cloudtrail-logs"
+    Environment = "dev"
+  }
+}
+
+
+resource "aws_s3_bucket_versioning" "cloudtrail_versioning" {
+  bucket = aws_s3_bucket.cloudtrail_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+resource "aws_s3_bucket_policy" "cloudtrail_policy" {
+  bucket = aws_s3_bucket.cloudtrail_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "AWSCloudTrailAclCheck"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action = "s3:GetBucketAcl"
+        Resource = aws_s3_bucket.cloudtrail_bucket.arn
+      },
+      {
+        Sid = "AWSCloudTrailWrite"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action = "s3:PutObject"
+        Resource = "${aws_s3_bucket.cloudtrail_bucket.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
+}
+
+
+
+data "aws_caller_identity" "current" {}
+
+
+resource "aws_cloudtrail" "main_trail" {
+  name                          = "tejas-cloudtrail"
+  s3_bucket_name                = aws_s3_bucket.cloudtrail_bucket.id
+  include_global_service_events = true
+  is_multi_region_trail         = true
+  enable_logging                = true
+
+  tags = {
+    Name = "main-cloudtrail"
+  }
+}
